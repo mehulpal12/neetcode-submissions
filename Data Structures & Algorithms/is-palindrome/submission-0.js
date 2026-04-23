73947d8749529811ef2a808d9bbff06a37136198
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-04-23 10:47:35 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let l = 0;
+        let r = s.length - 1;
+        while (l < r) {
+
+          while(l<r && !this.isAlphanumeric(s[l])){
+            l++
+          }
+          while(l<r && !this.isAlphanumeric(s[r])){
+            r--
+          }
+
+            if (s[l].toLowerCase() !== s[r].toLowerCase() ) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    isAlphanumeric(char) {
+        return (char >= 'a' && char <= 'z') || 
+               (char >= 'A' && char <= 'Z') || 
+               (char >= '0' && char <= '9');
+    }
+}
