aae86793d3f2450c79c1b4322585ae9fe193e965
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-04-30 10:46:13 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let res = {}
+        for(let i =0; i< strs.length; i++){
+        let sorted = strs[i].split('').sort().join('');
+        if(!res[sorted]){
+            res[sorted] = []
+        }
+         res[sorted].push(strs[i])
+        }
+        return Object.values(res)
+    }
+}
