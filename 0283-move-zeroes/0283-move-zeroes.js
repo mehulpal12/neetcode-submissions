e326613e2283e6039d4b88a1d302ca09ac0c880e
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-06-23 10:28:59 +0530

    Time: 0 ms (100%), Space: 61.3 MB (8.56%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (arr) {
+    let x = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !==  0) {
+            arr[x] = arr[i]
+            x++
+        }
+    }
+    for (let i = x; i < arr.length; i++) {
+        arr[i] = 0
+    }
+
+
+
+};
