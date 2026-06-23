70e9f287b358ae7b95ab2ae611a23b1b19ecfd56
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-06-23 22:24:22 +0530

    Add: longest-consecutive-sequence - submission-6

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-6.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(arr) {
+        let res = 0;
+        let store = new Set(arr);
+        for(let i = 0; i < arr.length; i++){
+            let streak = 0;
+            let curr = arr[i];
+            while(store.has(curr)){
+                streak++;
+                curr++
+            }
+            res = Math.max(res,streak)
+        }  
+        return res  
+    } 
+}
