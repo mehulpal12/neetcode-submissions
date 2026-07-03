887717fb98d0516cc98dd7b0f124ed454959da54
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-07-03 08:38:43 +0530

    Time: 5 ms (33.43%), Space: 54.4 MB (92.83%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -5,14 +5,15 @@
 var isValid = function(s) {
     let stack = [];
     for(let i = 0; i < s.length; i++){
-        if((s[i] === '{') || (s[i] === '(') || (s[i] === '[')){
+        if(s[i] === '{' || s[i] === "[" || s[i] === '('){
             stack.push(s[i])
         }else{
             let top = stack.pop();
-            if(!top || (top === '{' && s[i] !== '}') ||  (top === '(' && s[i] !== ')') ||  (top === '[' && s[i] !== ']')){
+            if(!top || (top === "{" && s[i] !== "}") || (top === "(" && s[i] !== ")") || (top === "[" && s[i] !== "]")){
                 return false
             }
         }
     }
-    return stack.length === 0;
+    return stack.length === 0
+    
 };
