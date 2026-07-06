60500d20dfb187eeaf2786755a204a674107dc18
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-07-06 16:04:34 +0530

    Time: 0 ms (100%), Space: 57.8 MB (62.29%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let x = 0;
+   let x = 0;
     for(let i = 0; i<nums.length;i++){
         if(nums[i] !== nums[x]){
             x++;
