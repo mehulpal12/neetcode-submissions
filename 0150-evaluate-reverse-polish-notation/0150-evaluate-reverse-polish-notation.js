5a23be3ecd8d2c1ec7f5039b4f81374708d3da31
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-07-10 11:07:17 +0530

    Time: 21 ms (19.54%), Space: 59.5 MB (23.36%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(arr) {
+    let stack = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === "+" || arr[i] === "-" || arr[i] === "*" ||arr[i] === "/"  ){
+            let a = stack.pop();
+            let b = stack.pop();
+            let val = eval(`${b} ${arr[i]} ${a}`)
+            stack.push(Math.trunc(val))
+        }else{
+            stack.push(arr[i])
+        }
+    }
+    return Number(stack.pop())
+    
+};
