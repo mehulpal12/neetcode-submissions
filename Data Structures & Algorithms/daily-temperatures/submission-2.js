5a9097b88972488d57123b464851c05aff83ba5f
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-07-13 12:08:09 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(arr) {
+        let n = arr.length;
+        let ans = new Array(n).fill(0);
+        let stack = [];
+        stack.push(n-1);
+        for(let i = n-2; i >=0; i--){
+            while(stack.length){
+                let top = stack[stack.length-1]
+                if(arr[i] >= arr[top]){
+                    stack.pop()
+                }else{
+                    ans[i] = top - i;
+                    break;
+                }
+            }
+            stack.push(i)
+        }
+        return ans
+    }
+}
