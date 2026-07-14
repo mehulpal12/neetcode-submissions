2b272a72f127d7fd6b24f0c27d4470af8bc3ed48
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-07-14 11:15:41 +0530

    Time: 9 ms (53.35%), Space: 57.4 MB (74.7%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var orangesRotting = function (grid) {
+    let m = grid.length;
+    let n = grid[0].length;
+
+    let queue = [];
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 2) {
+                queue.push([i, j, 0])
+            }
+        }
+    }
+    let count = 0
+
+    while (queue.length) {
+        let [x, y, level] = queue.shift();
+
+        // FIX: Replaced grid[x, y] with grid[x][y] everywhere below
+        if (x > 0 && grid[x - 1][y] === 1) {
+            grid[x - 1][y] = 2;
+            queue.push([x - 1, y, level + 1])
+        }
+        if (x < m - 1 && grid[x + 1][y] === 1) {
+            grid[x + 1][y] = 2;
+            queue.push([x + 1, y, level + 1])
+        }
+        if (y < n - 1 && grid[x][y + 1] === 1) {
+            grid[x][y + 1] = 2;
+            queue.push([x, y + 1, level + 1])
+        }
+        if (y > 0 && grid[x][y - 1] === 1) {
+            grid[x][y - 1] = 2;
+            queue.push([x, y - 1, level + 1])
+        }
+        count = Math.max(level, count)
+    }
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 1) {
+                return -1
+            }
+        }
+    }
+    return count
+};
