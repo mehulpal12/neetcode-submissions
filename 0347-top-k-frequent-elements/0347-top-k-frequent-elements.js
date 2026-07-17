9b6a5e00a1edc8bd2769201a05e3b0dcda8e0ded
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-07-17 10:41:34 +0530

    Time: 38 ms (5.06%), Space: 60.3 MB (48.48%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (arr, k) {
+    // push all the elem into map
+    let map = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (!map[arr[i]]) {
+            map[arr[i]] = 0;
+        }
+        ++map[arr[i]];
+    }
+    // push top k elem in to MinPQ;
+    let pq = new MinPriorityQueue(x => x.freq);
+    for (key in map) {
+        pq.push({ val: key, freq: map[key] })
+        if (pq.size() > k) {
+            pq.dequeue()
+        }
+    }
+    // return the most freq elem into a number array
+    return pq.toArray().map(x => Number(x.val))
+
+};
