e4442128eda4a06ec6cebcb1ff8c7cd0b1b71714
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-07-22 11:38:30 +0530

    Time: 0 ms (100%), Space: 51.6 MB (99.31%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -2,14 +2,16 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findPeakElement = function(nums) {
+var findPeakElement = function (nums) {
     let l = 0;
-    let r = nums.length-1;
-    while(l<r){
-        let m = l + Math.floor((r-l) / 2);
-        if(nums[m] < nums[m+1]){
-            l = m+1
-        }else{
+    let r = nums.length - 1;
+    if (r < 1) return 0
+    while (l < r) {
+        let m = Math.floor(l + (r - l) / 2);
+        if (nums[m] < nums[m + 1]) {
+            l = m + 1
+        }
+        else {
             r = m
         }
     }
