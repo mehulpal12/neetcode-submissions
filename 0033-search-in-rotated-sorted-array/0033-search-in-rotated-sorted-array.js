47604984b40e091832bcc4653a9f2aa585703b38
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-07-22 09:52:37 +0530

    Time: 0 ms (100%), Space: 55.4 MB (6.5%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -3,23 +3,23 @@
  * @param {number} target
  * @return {number}
  */
-var search = function (arr, x) {
+var search = function(arr, x) {
     let l = 0;
-    let r = arr.length - 1;
-    while (l <= r) {
-        let m = Math.floor(l + (r - l) / 2)
-        if (arr[m] === x) return m
-        if (arr[l] <= arr[m]) { // check the left side is sorted or not
-            if (arr[l] <= x && x < arr[m]) { // find the x is shorter then m
+    let r = arr.length-1;
+    while(l <= r){
+        let m = Math.floor(l + (r-l) / 2);
+        if(arr[m] === x) return m;
+        if(arr[l] <= arr[m]){
+            if(arr[l] <= x && x <= arr[m]){
                 r = m - 1
-            } else {
-                l = m + 1
+            }else{
+                l = m +1
             }
-        } else {
-            if (arr[m] < x && x <= arr[r]) {  // find the x is shorter then r
-                l = m + 1
-            } else {
-                r = m - 1
+        }else{
+            if(arr[m] <= x && x <= arr[r]){
+                l = m+1
+            }else{
+                r = m-1
             }
         }
     }
