86a256d9381d9da5eb2616052a24f5dd4be85536
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-07-31 11:04:29 +0530

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(arr, target) {
+        let result = [];
+
+        let backtrack = (remainSum,path,start) =>{
+            if(remainSum === 0){
+                result.push([...path])
+            }
+            if(remainSum <= 0) return
+            for(let i = start; i< arr.length; i++){
+                path.push(arr[i])
+                backtrack(remainSum-arr[i], path,i)
+                path.pop()
+            }
+        }
+        backtrack(target,[],0)
+        return result
+    }
+}
