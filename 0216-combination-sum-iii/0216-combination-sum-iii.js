5fcd6c0d487e2937713e08147ec4bdce375a87cb
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-07-31 10:49:09 +0530

    Time: 0 ms (100%), Space: 54 MB (59.31%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number[][]}
+ */
+var combinationSum3 = function(k, n) {
+    let result = [];
+
+    let backtrack = (remainSum,path,start) =>{
+        if(remainSum === 0 && path.length === k){
+            result.push([...path])
+        }
+        if(path.length === k){
+            return
+        }
+        for(let i = start; i<= 9; i++){
+            path.push(i)
+            backtrack(remainSum-i,path, i+1)
+            path.pop()
+        }
+    }
+    backtrack(n,[],1)
+    return result
+};
