9cb76d96e95643390170dbbce782b55742bdde91
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-08-04 11:32:45 +0530

    Time: 15 ms (80.43%), Space: 79.9 MB (82.72%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.js
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.js
@@ -2,32 +2,29 @@
  * @param {string} s
  * @return {string[][]}
  */
-var partition = function (s) {
-    let result = []
-    let isPalindrome = (s) => {
+var partition = function(s) {
+    let result = [];
+    let isPalindrome = (s) =>{
         let l = 0;
-        let r = s.length - 1;
-        while (l < r) {
-            if (s[l++] != s[r--]) return false
+        let r = s.length-1;
+        while(l<r){
+            if(s[l++] != s[r--])return false
         }
         return true
     }
-    let backtrack = (path, remainstr) => {
+    let backtrack = (path,remainstr) =>{
         if(!remainstr.length){
             result.push([...path])
             return
         }
-        for(let i = 1; i <= remainstr.length;i++){
-
-        let choice = remainstr.substring(0,i)
-        if(!isPalindrome(choice)){
-            continue;
-        }
-        path.push(choice);
-        backtrack(path, remainstr.substring(i))
-        path.pop()
+        for(let i = 1; i <= remainstr.length; i++){
+            let choice = remainstr.substring(0,i)
+            if(!isPalindrome(choice)) continue;
+            path.push(choice)
+            backtrack(path, remainstr.substring(i))
+            path.pop()
         }
     }
-    backtrack([], s)
+    backtrack([],s)
     return result
 };
