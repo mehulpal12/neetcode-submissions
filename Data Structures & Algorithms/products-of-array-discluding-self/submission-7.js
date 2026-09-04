48dc4ed136f1e165c5124bda3203f82a62097ad1
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-09-04 11:56:43 +0530

    Add: products-of-array-discluding-self - submission-7

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-7.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let n = nums.length;
+        let prefix = new Array(n).fill(1)
+        let suffix = new Array(n).fill(1)
+        let res = new Array(n).fill(1)
+
+        for(let i = 1; i < n; i++){
+            prefix[i] = prefix[i-1] * nums[i-1]
+        }
+        for(let i = n-2; i >= 0; i--){
+            suffix[i] = suffix[i+1] * nums[i+1]
+        }
+        for(let i = 0; i < n; i++){
+            res[i] = prefix[i] * suffix[i]
+        }
+        return res
+    }
+}
