a84c72bec9570a905725dad7a2acb8099018a280
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-09-09 09:51:54 +0530

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let l = 0;
+        let r = nums.length-1;
+        while(l < r){
+            if(nums[l] + nums[r] == target){
+                return [l+1,r+1]
+            }else if(nums[l] + nums[r] > target){
+                r--
+            }else{
+                l++
+            }
+        }
+    }
+}
