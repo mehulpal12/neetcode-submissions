8b05d90a21de99244384039165d4425d7b239555
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-09-09 09:25:46 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let encode = "";
+        for(let i = 0; i < strs.length; i++){
+            encode += strs[i].length + "#" + strs[i]
+        }
+        return encode
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(encode) {
+        let i = 0;
+        let res = [];
+        while(i < encode.length){
+            let j = i;
+            while(encode[j] !== "#"){
+                j++
+            }
+            let num = Number(encode.substring(i,j));
+            i = j+1
+            let word = encode.substring(i, i + num);
+            res.push(word);
+            i+= num
+        }
+        return res
+    }
+}
