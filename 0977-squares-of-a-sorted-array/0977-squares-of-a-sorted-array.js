ab3c00d01d894de80f19019ba1d8cb0c35b3aef3
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-09-15 09:27:05 +0530

    Time: 1 ms (99.22%), Space: 62.9 MB (26.1%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -3,10 +3,22 @@
  * @return {number[]}
  */
 var sortedSquares = function(arr) {
-    Math.abs(arr);
-    for(let i = 0; i < arr.length; i++){
-        arr[i] = arr[i] * arr[i]
+    let n = arr.length
+    let l = 0;
+    let r = n-1;
+    let index = n-1;
+    let res = new Array(n)
+    while(l <= r){
+        let leftS = arr[l] * arr[l];
+        let rightS = arr[r] * arr[r];
+        if(leftS > rightS){
+            res[index] = leftS;
+            l++
+        }else{
+            res[index] = rightS;
+            r--
+        }
+        index--
     }
-    let arrSort = arr.sort((a,b) => a-b);
-    return arrSort
+    return res
 };
