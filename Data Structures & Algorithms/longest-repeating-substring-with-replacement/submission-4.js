0b7175124f3416098a14bdd2df1df70f20939b4e
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-09-18 17:43:10 +0530

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    
+    characterReplacement(s, k) {
+        let res = 0;
+        let map = new Map();
+        let l = 0;
+        let maxf = 0;
+        for(let r = 0; r < s.length; r++){
+            map.set(s[r], (map.get(s[r]) || 0) + 1);
+            maxf = Math.max(maxf, map.get(s[r]))
+
+            while(r-l+1 - maxf > k){
+                map.set(s[l], map.get(s[l]) -1);
+                l++
+            }
+            res = Math.max(res, r-l+1)
+        }
+        return res
+    }
+}
